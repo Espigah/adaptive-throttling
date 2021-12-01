86e5fc27c738b9d6f14057e3bd161ad2a837a616
Espigah <fabricio.goncalves@m4u.com.br>
2021-12-01 09:30:32 -0300

    first commit :tada:

---
+++ __tests__/adaptive-throttling.test.ts
@@ -0,0 +1,80 @@
+import { createAdaptiveThrottling } from '../src/index';
+
+const exception = () => {
+  return new Promise((resolve, reject) => {
+    throw new Error('some error');
+  });
+};
+
+const success = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('success');
+    }, 1000);
+  });
+};
+
+test('Adaptive Throttling : success case', async () => {
+  const data = await createAdaptiveThrottling().execute(success);
+  expect(data).toEqual('success');
+});
+
+test('Adaptive Throttling : exception case', async () => {
+  const action = async () => {
+    await createAdaptiveThrottling().execute(exception);
+  };
+
+  await expect(action()).rejects.toThrow('some error');
+});
+
+test('Adaptive Throttling : out of quota case', async () => {
+  const adaptiveThrottling = createAdaptiveThrottling(0.1, 0.2, 0.8);
+  await adaptiveThrottling.execute(success);
+  for (let i = 0; i < 19; i++) {
+    try {
+      await adaptiveThrottling.execute(() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            reject('reject');
+          }, 100);
+        });
+      });
+    } catch (error) {}
+  }
+
+  expect(adaptiveThrottling.getCutoffIsReached()).toEqual(true);
+});
+
+// test('Adaptive Throttling : out of quota case', async () => {
+//   const adaptiveThrottling = createAdaptiveThrottling({
+//     historyTime: 0.1,
+//     timesAsLargeAsAccepts: 0.2,
+//     chanceOfRejectingNewRequesLimit: 0.8,
+//   });
+//   await adaptiveThrottling.execute(success);
+//   for (let i = 0; i < 19; i++) {
+//     try {
+//       await adaptiveThrottling.execute(() => {
+//         return new Promise((resolve, reject) => {
+//           setTimeout(() => {
+//             reject('reject');
+//           }, 1);
+//         });
+//       });
+//     } catch (error) {}
+//   }
+
+//   for (let i = 0; i < 1; i++) {
+//     try {
+//       await adaptiveThrottling.execute(() => {
+//         return new Promise((resolve, reject) => {
+//           setTimeout(() => {
+//             resolve('resolve');
+//           }, 1000);
+//         });
+//       });
+//     } catch (error) {}
+//   }
+
+//   expect(adaptiveThrottling.getCutoffIsReached()).toEqual(true);
+// });
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+function createAdaptiveThrottlingHistory(
+  historyTime = 2,
+  timesAsLargeAsAccepts = 2,
+  chanceOfRejectingNewRequesLimit = 0.9,
+) {
+  let requestsHistory: Date[] = [];
+  let acceptsHistory: Date[] = [];
+  let cutoffIsReached: boolean;
+  const checkDate = (historyTime: number) => {
+    const currentDate = new Date();
+    return new Date(currentDate.getTime() - historyTime * 60000);
+  };
+
+  const filterHistory = (value: Date) => {
+    const date = checkDate(historyTime);
+    return value > date;
+  };
+
+  return {
+    addRequests() {
+      requestsHistory.push(new Date(Date.now()));
+      return this;
+    },
+    addAccepts() {
+      acceptsHistory.push(new Date(Date.now()));
+      return this;
+    },
+    refresh() {
+      requestsHistory = requestsHistory.filter(filterHistory);
+      acceptsHistory = acceptsHistory.filter(filterHistory);
+
+      const requests = requestsHistory.length;
+      const accepts = acceptsHistory.length;
+
+      const p0 = Math.max(0, (requests - timesAsLargeAsAccepts * accepts) / (requests + 1)); // https://sre.google/sre-book/handling-overload/#eq2101
+      const p1 = Math.min(p0, chanceOfRejectingNewRequesLimit); // https://rafaelcapucho.github.io/2016/10/enhance-the-quality-of-your-api-calls-with-client-side-throttling/
+
+      //    console.log(p0, p1, cutoffIsReached, requests, accepts);
+
+      return (cutoffIsReached = p1 >= chanceOfRejectingNewRequesLimit);
+    },
+    getCutoffIsReached() {
+      return cutoffIsReached;
+    },
+  };
+}
+
+/**
+ *
+ * @param {Number} historyTime - Each client task keeps the following information for the last N minutes of its history:
+ * @param {Number} timesAsLargeAsAccepts - Clients can continue to issue requests to the backend until requests is K times as large as accepts
+ * @param {Number} chanceOfRejectingNewRequesLimit - Allowing the client to recover even in that worst scenario, when the service is down more than (historyTime) minutes.
+ * @returns
+ */
+export const createAdaptiveThrottling = (
+  historyTime: number = 2,
+  timesAsLargeAsAccepts: number = 2,
+  chanceOfRejectingNewRequesLimit: number = 0.9,
+) => {
+  const adaptiveThrottling = createAdaptiveThrottlingHistory(
+    historyTime,
+    timesAsLargeAsAccepts,
+    chanceOfRejectingNewRequesLimit,
+  );
+
+  return {
+    getCutoffIsReached() {
+      return adaptiveThrottling.getCutoffIsReached();
+    },
+    async execute(func: any) {
+      adaptiveThrottling.addRequests();
+
+      if (adaptiveThrottling.getCutoffIsReached()) {
+        adaptiveThrottling.addAccepts().refresh();
+        throw new Error('Out of quota');
+      }
+      try {
+        const result: any = await func();
+        adaptiveThrottling.addAccepts().refresh();
+        return result;
+      } catch (error) {
+        adaptiveThrottling.refresh();
+        throw error;
+      }
+    },
+  };
+};
